018df0c887568ab6a5e41c92f597d689ea5525c9
Giuseppe Romano <gromano@pop-os.localdomain>
2019-08-07 13:23:39 -0400

    cleaning

--- recipe/info/recipe/build_docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t romanodev/openbte .
-docker push romanodev/openbte
--- recipe/openbte/build_conda.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-conda config --set anaconda_upload no
-conda build --numpy=1.14 -c guyer/label/hidden -c conda-forge --python 3.6 .
