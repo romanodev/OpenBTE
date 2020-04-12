3212e244442dc365860a21fe3ec56b59a4267e2c
Giuseppe Romano <romanodev@pop-os.localdomain>
2020-04-12 18:40:07 -0400

    test

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(name='openbte',
                         'ipykernel',
                         'ipyleaflet',
                         'jupyter_client',
+                        'jupyter_sphinx',
                         'recommonmark',
                         'sphinx>=1.4.6',
                         'sphinx_rtd_theme'
