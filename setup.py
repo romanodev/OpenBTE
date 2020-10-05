2a6e85a3ef9dafdea3cf593e702f37fb24be0016
Giuseppe Romano <romanodev@pop-os.localdomain>
2020-10-05 16:15:39 -0400

    bug fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 import os
 
 setup(name='openbte',
-      version='1.77',
+      version='1.78',
       description='Boltzmann Transport Equation for Phonons',
       author='Giuseppe Romano',
       author_email='romanog@mit.edu',
