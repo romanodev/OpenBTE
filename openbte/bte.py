Fixed Fluxes' Units; changed the reference angle for the interpolation

openbte/bte.py
@@ -14,8 +14,8 @@ import numpy as np
 import time
 from nptyping import NDArray, Shape, Float, Int
 
-set_start_method('fork',force=True)
 
+set_start_method('fork',force=True)
 def BTE_RTA(geo                               : Mesh,\
             mat                               : Material,\
             bcs                               : BoundaryConditions,\
@@ -24,7 +24,7 @@ def BTE_RTA(geo                               : Mesh,\
             heat_source                       : HeatSource = None,\
             direct                            : bool  = True,\
             bte_max_iter                      : Int   = 100,\
-            bte_rtol                          : Float = 1e-5)->SolverResults:
+            bte_rtol                          : Float = 1e-4)->SolverResults:
 
     #parse material data
     mfps = mat.mfps
@@ -34,7 +34,6 @@ def BTE_RTA(geo                               : Mesh,\
         n_angles = mat.grid[1] * mat.grid[2]
     else:    
         n_angles = mat.grid[1] 
-
 
     #----------
 
@@ -70,16 +69,19 @@ def BTE_RTA(geo                               : Mesh,\
      s_diffuse = geo.side_physical_regions[bcs.diffuse] #Side of the diffuse boundary
      i_diffuse  = i[s_diffuse] 
      db      = geo.normal_areas[s_diffuse]/geo.elem_volumes[i_diffuse][:,np.newaxis]
+
      Gb      = np.einsum('mqj,sj->mqs',mat.sigma[:,:,:geo.dim],db,optimize=True)
-     Gbp2    = Gb.clip(min=0)
-     tmp     = Gb.clip(max=0).sum(axis=0).sum(axis=0) #Diffuse scattering
+     Gbp     = Gb.clip(min=0)
+     Gbm     = Gb.clip(max=0)
+     tmp     = Gbm.sum(axis=0).sum(axis=0) #Diffuse scattering
      R       = np.divide(1, tmp, out=np.zeros_like(tmp), where=tmp!=0)
+     GG      = np.einsum('mqs,s->mqs',Gbp,R)
+
+     #Tranpsport--
      tmp     = np.einsum('rj,nj->rn',mat.vmfp[:,:geo.dim],db,optimize=True)  
      Gbp2    = tmp.clip(min=0); 
-     Gbm2 = tmp.clip(max=0)
+     Gbm2    = tmp.clip(max=0)
      np.add.at(D.T,i_diffuse,Gbp2.T)
-     Gbp = np.einsum('mqj,sj->mqs',mat.sigma[:,:,:geo.dim],db,optimize=True).clip(min=0)
-     GG = np.einsum('mqs,s->mqs',Gbp,R)
     else: 
      i_diffuse = np.empty(0)
     n_diffuse = len(i_diffuse)
@@ -98,11 +100,12 @@ def BTE_RTA(geo                               : Mesh,\
     for region,jump in bcs.periodic.items():
 
         sides =  geo.periodic_sides[region]
+
         tmp   =  geo.normal_areas[sides]/geo.elem_volumes[i[sides]][:,np.newaxis]
         tmp   =  np.einsum('rj,nj->rn',mat.vmfp,tmp,optimize=True)  
         np.add.at(P.T,i[sides],-(tmp.clip(max=0)*jump).T)
 
-        tmp   =  -geo.normal_areas[sides]/geo.elem_volumes[j[sides]][:,np.newaxis]
+        tmp   = -geo.normal_areas[sides]/geo.elem_volumes[j[sides]][:,np.newaxis]
         tmp   =  np.einsum('rj,nj->rn',mat.vmfp,tmp,optimize=True)  
         np.add.at(P.T,j[sides],(tmp.clip(max=0)*jump).T)
 
@@ -112,7 +115,6 @@ def BTE_RTA(geo                               : Mesh,\
     else: 
      P_nano = np.zeros(n_angles)   
  
-
     #Total Perturbation--
     perturbation = P + RHS_ISO + P_nano[:,np.newaxis]
     #--------------------
@@ -162,6 +164,7 @@ def BTE_RTA(geo                               : Mesh,\
      TB     = DeltaT[i_diffuse]
     else: 
      TB =   np.empty(0)   
+
     J = np.zeros((geo.n_elems,mat.vmfp.shape[1]))
 
     #Options
@@ -201,7 +204,6 @@ def BTE_RTA(geo                               : Mesh,\
          partial_J  = np.zeros_like(J)
          partial_K  = np.zeros_like(K)
          partial_S  = np.zeros_like(S)
-        
          T = np.zeros(n_elems)
          for n in inds:  
            for m,mfp in enumerate(mfps):
@@ -226,7 +228,7 @@ def BTE_RTA(geo                               : Mesh,\
                   np.add.at(partial_TB,np.arange(n_diffuse),-T[i_diffuse]*GG[m,n])
 
                  #Contribute to Flux
-                 partial_J += np.einsum('c,j->cj',T,mat.sigma[m,n])
+                 partial_J += np.einsum('c,j->cj',T-d['DeltaT'],mat.sigma[m,n])
 
                  if not effective_thermal_conductivity == None:
                   #Compute kappa_mode
@@ -264,9 +266,8 @@ def BTE_RTA(geo                               : Mesh,\
 
     kappa_eff = np.sum(sh['K'])
 
-    variables = {'Temperature_BTE':{'data':sh['DeltaT'],'units':'K'},'Flux_BTE':{'data':sh['J'],'units':'W/m/m'}}
-
-    #variables.update({'Vorticity_BTE':{'data':geo.vorticity(bcs,sh['J']),'units':'W/m/m/m'}})
+    variables = {'Temperature_BTE':{'data':sh['DeltaT'],'units':'K'},'Flux_BTE':{'data':sh['J']*1e9,'units':'W/m/m'}}
+        
 
     return SolverResults(kappa_eff = kappa_eff,variables=variables,aux = {'suppression':sh['S']})
                    

openbte/fourier.py
@@ -112,11 +112,10 @@ def Fourier(geometry                       : Mesh,\
      if modified_fourier:
       F += sp.eye(geometry.n_elems,format='csc')
      else:   
-      if not effective_thermal_conductivity == None:
-       #Fix a point for stability
-       scale = np.ones(geometry.n_elems); scale[N] = 0
-       F.data = F.data * scale[F.indices]
-       F[N,N] = 1
+         if len(boundary_conditions.mixed) == 0:
+           scale = np.ones(geometry.n_elems); scale[N] = 0
+           F.data = F.data * scale[F.indices]
+           F[N,N] = 1
        #-----------------------------
 
      lu = linalg.splu(F)
@@ -139,6 +138,7 @@ def Fourier(geometry                       : Mesh,\
     if modified_fourier:
        B += global_temperature
     else:   
+      if len(boundary_conditions.mixed) == 0:
        B[N] = 0
     #-----------------------
 
@@ -150,10 +150,11 @@ def Fourier(geometry                       : Mesh,\
 
         n_iter +=1
         T        = lu.solve(B+C)
+
         #Normalized T 
-        if (not effective_thermal_conductivity == None) and (not modified_fourier == True):
-         T -= (max(T) + min(T))*0.5
-        
+        if len(boundary_conditions.mixed) == 0 and not modified_fourier:
+           T -= (max(T) + min(T))*0.5
+
         gradient = geometry.gradient(boundary_conditions,T)
        
         #Nonorthogonal contribution (only on internal sides)
@@ -180,7 +181,7 @@ def Fourier(geometry                       : Mesh,\
 
     J = -np.einsum('ij,cj->ci',thermal_conductivity,gradient)
 
-    variables = {'Temperature_Fourier':{'data':T,'units':'K'},'Flux_Fourier':{'data':J,'units':'W/m/m'}}
+    variables = {'Temperature_Fourier':{'data':T,'units':'K'},'Flux_Fourier':{'data':J*1e8,'units':'W/m/m'}}
     return SolverResults(kappa_eff = kappa_eff,variables=variables)
 
 

openbte/material.py
@@ -15,8 +15,8 @@ def RTA3D(data : MaterialRTA,**kwargs)->Material:
     n_angles = n_phi * n_theta
     #Angular discretization
     Dphi = 2.0*np.pi/n_phi
-    #phi = np.linspace(Dphi/2.0,2.0*np.pi-Dphi/2.0,n_phi,endpoint=True)
-    phi = np.linspace(0,2.0*np.pi,n_phi,endpoint=False)
+    phi = np.linspace(Dphi/2.0,2.0*np.pi-Dphi/2.0,n_phi,endpoint=True)
+    #phi = np.linspace(0,2.0*np.pi,n_phi,endpoint=False)
     Dtheta = np.pi/n_theta
     theta = np.linspace(Dtheta/2,np.pi-Dtheta/2,n_theta,endpoint=True)
     #theta = np.linspace(0,np.pi,n_theta,endpoint=True)
@@ -111,9 +111,12 @@ def RTA2DSym(data : MaterialRTA,**kwargs)->Material:
 
     #Polar angle---
     Dphi = 2*np.pi/n_phi
+    factor = np.sinc(Dphi/2/np.pi)
+    
     #phi = np.linspace(Dphi/2,2.0*np.pi-Dphi/2,n_phi,endpoint=True)
     phi = np.linspace(0,2.0*np.pi,n_phi,endpoint=False)
-    polar_ave = np.array([np.sin(phi),np.cos(phi)]).T
+
+    polar_ave = np.array([np.sin(phi),np.cos(phi)]).T#*factor
     #----------------
 
     #Build mode-resolved quantities
@@ -123,21 +126,18 @@ def RTA2DSym(data : MaterialRTA,**kwargs)->Material:
     f        = np.divide(np.ones_like(tau),tau, out=np.zeros_like(tau), where=tau!=0)
     Wdiag    = data.heat_capacity*f
 
-    #Wdiag    = data.gamma
-    #mfp_bulk = data.vMFP
-    #sigma    = np.einsum('u,ui->ui',Wdiag,mfp_bulk[:,:2])
-    #quit()
-
     #Convert into polar space
     r_bulk,phi_bulk = utils.compute_polar(mfp_bulk)
 
     #Filtering small MFPs out (1e-10m)
-    I = np.where(r_bulk>1e-9)
+    I = np.where(r_bulk>1e-10)
     r_bulk = r_bulk[I]
     mfp_bulk = mfp_bulk[I]
     phi_bulk = phi_bulk[I]
     Wdiag = Wdiag[I]
     sigma = sigma[I]
+
+    #utils.save('aux',{'gamma':Wdiag,'vMPF':mfp_bulk})
 
     #Sampling
     mfp_max     = kwargs.setdefault('mfp_max',np.max(r_bulk)*1.1)
@@ -148,10 +148,10 @@ def RTA2DSym(data : MaterialRTA,**kwargs)->Material:
     Wdiag_sampled = np.zeros((n_mfp,n_phi))
     sigma_sampled = np.zeros((n_mfp,n_phi,2)) 
     #Interpolation in the MFPs
-    a1,a2,m1,m2 = utils.fast_interpolation(r_bulk,mfp_sampled,bound='extent')
+    a1,a2,m1,m2 = utils.fast_interpolation(r_bulk,mfp_sampled,bound='extent',scale='log')
     #Interpolation in phi---
     b1,b2,p1,p2 = utils.fast_interpolation(phi_bulk,phi,bound='periodic')
-    
+   
     np.add.at(Wdiag_sampled,(m1, p1),a1*b1*Wdiag)
     np.add.at(Wdiag_sampled,(m1, p2),a1*b2*Wdiag)
     np.add.at(Wdiag_sampled,(m2, p1),a2*b1*Wdiag)
@@ -177,6 +177,7 @@ def RTA2DSym(data : MaterialRTA,**kwargs)->Material:
 
     #Heat source ratio
     coeff = 1/np.sum(Wdiag)
+
 
     return Material(kappa_sampled,\
                     sigma_sampled*1e-9,\
@@ -214,20 +215,22 @@ def Gray3DEqui(**kwargs):
 
 def Gray2D(**kwargs):
 
-    n_phi = kwargs.setdefault('n_phi',48)
+    n_phi = kwargs.setdefault('n_phi',96)
     MFP   = kwargs['MFP'] #in nm
     Dphi = 2*np.pi/n_phi
-    phi = np.linspace(Dphi/2.0,2.0*np.pi-Dphi/2.0,n_phi,endpoint=True)
-    #phi = np.linspace(0,2.0*np.pi,n_phi,endpoint=False)
-
+    #phi = np.linspace(Dphi/2.0,2.0*np.pi-Dphi/2.0,n_phi,endpoint=True)
+    phi = np.linspace(0,2.0*np.pi,n_phi,endpoint=False)
 
     polar = np.array([np.sin(phi),np.cos(phi)]).T
+
     fphi= np.sinc(Dphi/2.0/np.pi)
     polar_ave = polar#*fphi
+    polar_ave = np.round(polar_ave,4)
     kappa = kwargs.setdefault('kappa',1)*np.eye(2)
 
     sigma = np.zeros((1,n_phi,2))
     sigma[0] =2*kappa[0,0]/MFP * polar_ave/n_phi
+
 
     n_angles = n_phi
     mfps = np.array([MFP])
@@ -240,7 +243,8 @@ def Gray2D(**kwargs):
     #Heat source coeff
     heat_source_coeff = MFP*MFP*0.5/kappa[0,0] 
 
-    return Material(kappa,sigma,polar_ave,t_coeff,mfps,n_angles,1,h,heat_source_coeff)
+    return Material(kappa,sigma,polar_ave,t_coeff,mfps,(1,n_angles),h,heat_source_coeff)
+
 
 def Gray3D(**kwargs):
 

openbte/mesh.py
@@ -51,12 +51,12 @@ def get_periodicity(nodes,lines,dim) :
                    return np.absolute(v)
 
 
-def get_mesh()->Mesh:
+def get_mesh(filename='mesh')->Mesh:
     """Build mesh with gmsh"""
 
     #Dimension
     #Import mesh
-    with open('mesh.msh', 'r') as f: lines = f.readlines()
+    with open(filename + '.msh', 'r') as f: lines = f.readlines()
     #Physical surfaces--
     lines = [l.split()  for l in lines]
     nb = int(lines[4][0])
@@ -313,7 +313,9 @@ def get_mesh()->Mesh:
              ind2 = list(elem_side_map[e2]).index(s)
              diff_dist[e2,ind2] =  -dists[s]
             else:
-             diff_dist[e1,ind1] = dists[s]
+                
+             #diff_dist[e1,ind1] = dists[s]
+             diff_dist[e1,ind1] = side_centroids[s] - elem_centroids[e1] 
 
     gradient_weights = np.linalg.pinv(diff_dist)
 

openbte/objects.py
@@ -7,7 +7,7 @@ import numpy as np
 import time
 from sqlitedict import SqliteDict
 from openbte import utils
-from scipy.interpolate import LinearNDInterpolator
+from scipy.interpolate import LinearNDInterpolator,griddata,interp2d
 import plotly.graph_objs as go
 
 f64   = np.float64
@@ -62,6 +62,12 @@ class SharedMemory:
           return variable['data'].value
        else:
           return np.frombuffer(variable['data'].get_obj(),dtype=variable['dtype']).reshape(variable['shape'])
+
+
+    def get_waits(self):
+
+         self.print(self.barrier.wait())
+
 
     def sync(self):
          """Barrier"""
@@ -165,10 +171,10 @@ class EffectiveThermalConductivity(NamedTuple):
 
      return P
 
+
     def get(self,T,geo,bcs,sigma,DeltaT):
            """Compute the effective thermal conductivity at the diffusive level"""
-
-           #Nanowire 
+           #Nanowire
            if bcs.is_nanowire:
 
               e1 = np.arange(geo.n_elems)
@@ -182,26 +188,30 @@ class EffectiveThermalConductivity(NamedTuple):
            else:
               bc   = bcs.get_bc_by_name(self.contact)
 
-              if bc == 'Dirichlet': 
-                 sides = geo.side_physical_regions[contact]
+              if bc == 'Mixed': 
+                 sides = geo.side_physical_regions[self.contact]
                  side_areas = np.linalg.norm(geo.normal_areas[sides],axis=1)
                  sigma_normal = np.einsum('i,ci->c',sigma,geo.normal_areas[sides])
                  e1 = geo.side_elem_map[sides][:,0]
-                 partial_K  =  np.dot(T[e1],sigma_normal[m,n].clip(min=0))
-                 partial_K += bcs.dirichlet[contact]*np.sum(sigma_normal.clip(max=0))
+                 partial_K  =  np.dot(T[e1],sigma_normal.clip(min=0))
+                 partial_K += bcs.mixed[self.contact]['value']*np.sum(sigma_normal.clip(max=0))
 
               else:   
                  sides = geo.periodic_sides[self.contact]
                  side_areas = np.linalg.norm(geo.normal_areas[sides],axis=1)
                  sigma_normal = np.einsum('i,ci->c',sigma,geo.normal_areas[sides])
+                 sm = sigma_normal.clip(max=0)
+                 sp = sigma_normal.clip(min=0)
+
                  e1 = geo.side_elem_map[sides][:,0]
                  e2 = geo.side_elem_map[sides][:,1]
-                 partial_K   =  np.dot(T[e1]-DeltaT[e1],sigma_normal.clip(min=0))
-                 partial_K  +=  np.dot(T[e2]-DeltaT[e2],sigma_normal.clip(max=0))
+                 partial_K   =  np.dot(T[e1]-DeltaT[e1],sp)
+                 partial_K  +=  np.dot(T[e2]-DeltaT[e2],sm)
                  #partial_K  +=  bcs.periodic[self.contact]*np.sum(sigma_normal.clip(max=0))
+                 #partial_K   =  np.dot(T[e1]-DeltaT[e1],sigma_normal)
 
-              partial_S   =  np.dot(T[e1]-DeltaT[e1],side_areas)*0.5
-              partial_S  +=  np.dot(T[e2]-DeltaT[e2],side_areas)*0.5
+
+              partial_S   =  np.dot(T[e1]-DeltaT[e1],side_areas)
 
               return partial_K*self.normalization,partial_S*self.normalization   
 
@@ -218,6 +228,7 @@ class BoundaryConditions(NamedTuple):
 
     is_nanowire  : bool = False
 
+    
     def get_bc_by_name(self,name : str):
 
       if name in self.mixed.keys():
@@ -329,11 +340,6 @@ class Mesh(NamedTuple):
             diff_data[i,ind1] =   delta
             diff_data[j,ind2] =  -delta
 
-            #A[i,i,ind1] =  -1
-            #A[i,j,ind1] =   1
-            #A[j,i,ind2] =   1
-            #A[j,j,ind2] =  -1
-
      #B = np.zeros((self.n_elems,len(self.elems[0])))   
      #Periodic boundaries
      if jump:
@@ -353,7 +359,6 @@ class Mesh(NamedTuple):
 
      #t = np.einsum('kls,l->ks',A,variable)
      #print(np.allclose(t,diff_data))
-     #quit()
 
      #C = np.einsum('kjs,ks->kj',self.gradient_weights,B)
 
@@ -362,11 +367,8 @@ class Mesh(NamedTuple):
      #gradient = np.einsum('kjl,l->kj',T,variable) + C
 
 
-     gradient =  np.array([np.einsum('js,s->j',self.gradient_weights[k],dt) for k,dt in enumerate(diff_data)])
 
      #print(np.allclose(gradient,gradient2))
-
-
 
      #Let's attempt to build a matrix--
 
@@ -390,12 +392,15 @@ class Mesh(NamedTuple):
      #print(np.max(diff_data))
      #quit()
      #Dirichlet boundaries
-     #for key,value in bcs.dirichlet.items():
-     #   for s in mesh.side_physical_regions[key]: 
-     #     i = mesh.side_elem_map[s][0]  #i is the element
-     #     delta = value - variable[i]
-     #     ind    = list(mesh.elem_side_map[i]).index(s)
-     #     diff_data[i,ind] =  delta
+     for key,value in bcs.mixed.items():
+         for s in self.side_physical_regions[key]: 
+          i = self.side_elem_map[s][0]  #i is the element
+          delta = value['value'] - variable[i]
+          ind    = list(self.elem_side_map[i]).index(s)
+          diff_data[i,ind] =  delta
+
+
+     gradient =  np.array([np.einsum('js,s->j',self.gradient_weights[k],dt) for k,dt in enumerate(diff_data)])
 
      return gradient
 
@@ -471,9 +476,8 @@ class Mesh(NamedTuple):
         for k,elem in enumerate(elems):
          conn[elem] +=1
          for key,value in node_variables.items():
-          value['data'][elem[0]] += cell_variables[key]['data'][k]
-          value['data'][elem[1]] += cell_variables[key]['data'][k]
-          value['data'][elem[2]] += cell_variables[key]['data'][k]
+          for i in elem:
+           value['data'][i] += cell_variables[key]['data'][k]
 
         #Normalization
         for key,value in node_variables.items():
@@ -549,19 +553,15 @@ class OpenBTEResults(NamedTuple):
 
 
     def plot_over_line(self,**kwargs):
-        """Plot over line"""
+        """Plot over a line"""
 
         #Get options--
-        #N           = kwargs.setdefault('N',100)
         direction   = kwargs.setdefault('direction','x')
-        cut         = kwargs.setdefault('cut',0)
         repeat      = kwargs.setdefault('repeat',[1,1,1])
 
         variables   = kwargs['variables']
-        #p1          = kwargs['p1']
-        #p2          = kwargs['p2']
-        x          = kwargs['x']
-        N          = len(x)
+        x           = np.array(kwargs['x'])
+        N           = len(x)
 
         #Collect results variables--
         cell_variables = self.get_variables()
@@ -570,18 +570,9 @@ class OpenBTEResults(NamedTuple):
 
         node_variables = self.mesh.cell_to_node(cell_variables,nodes,elems)
 
-        #adjust points 
-        #delta = 1e-4
-        #p1 = np.array(p1)
-        #p2 = np.array(p2)
-        #p1 = p1 + delta*(p2-p1)
-        #p2 = p2 - delta*(p2-p1)
-        #x = p1[np.newaxis,:] + np.einsum('i,k->ki',(p2-p1),np.arange(N))/(N-1)
-
-        x = np.array(x) 
         output = {variable:[] for variable in variables}
         for v,variable in enumerate(variables):
-            output[variable] = LinearNDInterpolator(nodes,node_variables[variable]['data'])(x[:,0],x[:,1])
+            output[variable] = griddata(nodes,node_variables[variable]['data'],x)
 
         #Compute x    
         linear_x = np.zeros(N)
@@ -656,6 +647,7 @@ class OpenBTEResults(NamedTuple):
        for n,(key, value) in enumerate(variables.items()):
 
         name = key + '[' + value['units'] + ']'
+
         if value['data'].ndim == 1: #scalar
              strc +='SCALARS ' + name + ' double\n'
              strc +='LOOKUP_TABLE default\n'

openbte/utils.py
@@ -12,6 +12,74 @@ import functools
 import scipy
 from scipy.ndimage import rotate
 import joblib
+import subprocess,os
+
+def _regularize(phi):
+
+    phi[phi<0]       += 2*np.pi
+    phi[phi>2*np.pi] -= 2*np.pi
+
+    return phi
+
+def _n(phi):
+
+   a = np.sin(phi)
+   b = np.sin(phi)
+   if phi.ndim == 1:
+    return np.stack((np.sin(phi),np.cos(phi))).T
+   else:
+    return np.swapaxes(np.stack((np.sin(phi),np.cos(phi))).T,0,1)
+
+
+def generate_mesh_2D(filename='mesh'):
+    """Generate 2D mesh"""
+
+    with open(os.devnull, 'w') as devnull:
+          output = subprocess.check_output(("gmsh -format msh2 -2 " + filename + ".geo -o " + filename + ".msh").split(), stderr=devnull)
+
+
+def split(S,B,normalize=True):
+
+    phi_B   = _regularize(np.arctan2(B[:,0],B[:,1]))
+    phi_S   = _regularize(np.arctan2(S[...,0],S[...,1]))
+    Dphi    = phi_S[...,1]-phi_S[...,0]
+    if Dphi.ndim == 1:
+        Dphi = Dphi[0]
+
+    #Compute magnitude---------------
+    mag_s = np.linalg.norm(S,axis=-1)
+    mag_B = np.linalg.norm(B,axis=-1)
+    mag   = np.einsum('...,k->...k',mag_s,mag_B)
+    #---------------------------------
+
+    delta = 1e-4
+
+    DeltaPhi = _regularize(phi_S[...,np.newaxis] - phi_B[np.newaxis,:])
+
+    #This does a rotation automatically
+    ap = np.einsum('...,...i,ki->...k',mag_s,_n(phi_S + Dphi/2 - np.pi/2),B)
+    am = np.einsum('...,...i,ki->...k',mag_s,_n(phi_S - Dphi/2 - np.pi/2),B)
+    a  = ap-am
+    gp = a.clip(min=0)
+    gm = a.clip(max=0)
+
+    #Correction at n + np.pi/2
+    r  = np.logical_and(DeltaPhi >= np.pi/2-Dphi/2+delta,DeltaPhi <= np.pi/2 + Dphi/2-delta)
+    gp[r.nonzero()] =  (mag  - am)[r.nonzero()]
+    gm[r.nonzero()] =  (ap   - mag)[r.nonzero()]
+
+    #Correction at n - np.pi/2
+    r  = np.logical_and(DeltaPhi >= 3*np.pi/2-Dphi/2+delta,DeltaPhi <= 3*np.pi/2 + Dphi/2-delta)
+    gp[r.nonzero()] =   (ap  + mag)[r.nonzero()]
+    gm[r.nonzero()] =  -(mag + am)[r.nonzero()]
+
+    factor = 1/Dphi if normalize else 1
+
+
+    return gm*factor,\
+           gp*factor
+
+    
 
 
 def solver(LL,P,x0,callback = lambda x:x,verbose=False,maxiter=200,early_termination=False,tol=1e-4,inplace=True,filename = None,MM=None):
@@ -124,8 +192,8 @@ def run(target          : Callable, \
     import sys
     parser = argparse.ArgumentParser()
     parser.add_argument('-np', help='Number of processors',default=multiprocessing.cpu_count())
-    n_process = int(parser.parse_args().np)
-    
+    args = parser.parse_args("")
+    n_process = int(args.np)
     print('n_process: ',n_process)
 
     #Init shared variables
@@ -203,10 +271,10 @@ def load(filename,source='database'):
     print('{} does not exist'.format(full_name))
     quit()
 
-
  return data      
 
 def fast_interpolation(fine,coarse,bound=False,scale='linear') :
+ """Vectorized Interpolation"""
 
  if scale == 'log':
    fine    = np.log10(fine)